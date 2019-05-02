9baf23f8d4f9b62290d2c2fe69522a54d2bc4e24
Niloy Saha <niloysaha@gmail.com>
2019-05-01 17:12:53 -0700

    sample

---
+++ src/main/java/com/programming/leetcode/ReverseInteger.java
@@ -0,0 +1,23 @@
+package com.programming.leetcode;
+
+/**
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ * https://leetcode.com/problems/reverse-integer/
+ */
+public class ReverseInteger {
+    private static int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(Integer.MAX_VALUE));
+    }
+}
