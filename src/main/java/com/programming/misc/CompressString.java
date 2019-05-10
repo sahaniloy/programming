ee2bdb7f19cbf5b9fe0f556d5aa15160df50d444
Niloy Saha <niloysaha@gmail.com>
2019-05-09 17:32:01 -0700

    New code added

--- src/main/java/com/programming/misc/CompressString.java
+++ src/main/java/com/programming/misc/CompressString.java
@@ -22,6 +22,6 @@ public class CompressString {
     }
 
     public static void main(String[] args) {
-        System.out.println(compress("abccccccccccccc"));
+        System.out.println(compress("aaaabbcccdddddd"));
     }
 }
