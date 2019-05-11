7da5353c122a41a39848e82b38cf770460906e07
Niloy Saha <32936923+sahaniloy@users.noreply.github.com>
2019-05-11 11:14:23 -0700

    merge two strings code completed

--- src/main/java/com/programming/misc/MergeTwoStrings.java
+++ src/main/java/com/programming/misc/MergeTwoStrings.java
@@ -6,12 +6,27 @@ package com.programming.misc;
 public class MergeTwoStrings {
 
     private static String mergeTwoStrings(String s1, String s2) {
-        String result = "";
+        StringBuilder builder = new StringBuilder();
 
-        return result;
+        int i = 0, j = 0;
+
+        while (i < s1.length() && j < s2.length()) {
+            builder.append(s1.charAt(i++));
+            builder.append(s2.charAt(j++));
+        }
+
+        if (i != s1.length())
+            builder.append(s1.substring(i));
+
+        if (j != s2.length())
+            builder.append(s2.substring(j));
+
+        return builder.toString();
     }
 
     public static void main(String[] args) {
-
+        String s1 = "geeks";
+        String s2 = "forgeeks";
+        System.out.println(mergeTwoStrings(s1, s2));
     }
 }
