774fa519690cf401ba7c560cd356885bd2540b7a
Niloy Saha <niloysaha@gmail.com>
2019-05-16 17:40:53 -0700

    Renamed

--- src/main/java/com/testing/TestClass.java
+++ src/main/java/com/testing/TestClass.java
@@ -1,5 +1,10 @@
 package com.testing;
 
+import org.apache.commons.lang.StringUtils;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 /**
  * Test class
  */
@@ -7,5 +12,18 @@ public class TestClass {
 
     public static void main(String[] args) {
         System.out.println("CPU core count : " + Runtime.getRuntime().availableProcessors());
+        System.out.println(getTransactionUUID());
+    }
+
+    public static String getTransactionUUID() {
+        String transId = null;
+        try {
+            SecureRandom uniqueRandom = SecureRandom.getInstance("SHA1PRNG");
+            String ranNumber = String.valueOf(Math.abs(uniqueRandom.nextLong())); //NOSONAR
+            transId = StringUtils.rightPad(ranNumber, 19, '0');
+        } catch (NoSuchAlgorithmException e) {
+            //need not to do anything
+        }
+        return transId;
     }
 }
