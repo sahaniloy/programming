f9c4cc1572378a4ac3d6f0a61bc2fe38af0204fb
Niloy Saha <niloysaha@gmail.com>
2019-05-28 16:16:58 -0700

    New code added

---
+++ src/main/java/com/programming/geeksforgeeks/PrintItemContainingAllChars.java
@@ -0,0 +1,41 @@
+package com.programming.geeksforgeeks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Print list items containing all characters of a given word
+ * https://www.geeksforgeeks.org/print-list-items-containing-all-characters-of-a-given-word/
+ */
+public class PrintItemContainingAllChars {
+
+    public static final int LOWECASE_ALPHABET_COUNT = 26;
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        list.add("sunday");
+        list.add("took");
+        list.add("unseason");
+        for (int i = 0; i < list.size(); i++) {
+            check(list.get(i), "sun");
+        }
+    }
+
+    static void check(String str, String ron) {
+        int matchedCharCount = 0;
+        int[] a = new int[LOWECASE_ALPHABET_COUNT];
+
+        for (int i = 0; i < str.length(); i++) {
+            a[str.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < ron.length(); i++) {
+
+            if (a[ron.charAt(i) - 'a'] >= 1)
+                matchedCharCount++;
+        }
+
+        if (matchedCharCount == ron.length())
+            System.out.println(str);
+    }
+}
---
+++ src/main/java/com/programming/hackerrank/CompareTriplet.java
@@ -0,0 +1,29 @@
+package com.programming.hackerrank;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author RS Software
+ */
+public class CompareTriplet {
+
+    public static void main(String[] args) {
+        System.out.println(compareTriplets(Arrays.asList(5, 6, 7), Arrays.asList(3, 6, 10)));
+    }
+
+    // Complete the compareTriplets function below.
+    private static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        int[] result = {0, 0};
+
+        for (int i = 0; i < a.size(); i++) {
+            if (a.get(i) > b.get(i))
+                result[0]++;
+            else if (a.get(i) < b.get(i))
+                result[1]++;
+            else if (a.get(i) == b.get(i))
+                continue;
+        }
+        return Arrays.asList(result[0], result[1]);
+    }
+}
